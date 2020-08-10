bf8efcbd3437952213c571eb0482ee5eaa5f8031
hisnameispum <sirasitxp@gmail.com>
2020-08-10 00:40:59 -0700

    structured my code into a new widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -8,18 +11,57 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-            color: Color(0xFF1D1E33)
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
         ),
-        width: 170,
-        height: 200,
-      ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(Color(0xFF1D1E33)),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color myColor;
+  ReusableCard(@required this.myColor);
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: myColor),
     );
   }
 }
